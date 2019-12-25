ed7f40c0101f38ebe921996db5ff008ebdd12c91
KOSaber <k.o.saber@hotmail.com>
2019-12-25 14:45:14 +0300

    second commit

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,13 +2,9 @@ class Book < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_and_belongs_to_many :users
 
-  validates :title,presence: true, uniqueness: false , length: { minimum: 3, too_short: "must write valid book name" }
-  validates :auther, :publisher, :edition, presence: true, uniqueness: false, format: { with: /\A[a-zA-Z]+\z/,
-  message: "only allows letters" } , length: { minimum: 3,maximum: 10, too_short: "must have at least %{count} letters" }
+  validates :title, :auther, :publisher, :edition, presence: true, uniqueness: false, length: { minimum: 3,maximum: 35, too_short: "must have at least %{count} letters" }
   validates :price, presence: true, uniqueness: false, numericality: true
-
   validates :pic, presence: true, uniqueness: false
-
   validates :status, presence: true, uniqueness: false, format: { with: /\A[a-zA-Z]+\z/,
-  message: "only allows letters" }, length: { minimum: 8,maximum: 15, too_short: "must have at least %{count} letters" }
+  message: "only allows letters" }, length: { minimum: 8,maximum: 25, too_short: "must have at least %{count} letters" }
 end
